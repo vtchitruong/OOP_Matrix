861166a4e73c720fc7355d291e53fa27eed16f1a
Chi Truong <vtchitruong@gmail.com>
2021-06-06 20:42:01 +0700

    add Order class

--- oop_matrix.cpp
+++ oop_matrix.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+class Order
+{
+private:
+    static int n;
+public:
+    void Input()
+    {
+        cout << "Current value of n is " << n << endl;
+        cout << "Input n: ";
+        cin >> n;
+    }
+
+    int getn()
+    {
+        return n;
+    }
+};
+
+
 int main()
 {
 
